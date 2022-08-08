9ba3497034538ef6f55d448d7586d9530843c709
pramilaniroshan <pramilaniroshan@gmail.com>
2022-08-08 08:10:56 +0530

    Spring boot app init

---
+++ src/main/java/com/entgra/entgratask/EntgraTaskApplication.java
@@ -0,0 +1,13 @@
+package com.entgra.entgratask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntgraTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntgraTaskApplication.class, args);
+    }
+
+}
