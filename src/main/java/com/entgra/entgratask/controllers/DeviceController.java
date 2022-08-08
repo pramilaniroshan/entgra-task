1c001932d74d8a97779f75ebfca7e27d42b7092c
pramilaniroshan <pramilaniroshan@gmail.com>
2022-08-08 10:09:03 +0530

    Add device route added

---
+++ src/main/java/com/entgra/entgratask/controllers/DeviceController.java
@@ -0,0 +1,28 @@
+package com.entgra.entgratask.controllers;
+
+import com.entgra.entgratask.entities.Device;
+import com.entgra.entgratask.services.DeviceService;
+import com.entgra.entgratask.utils.ResponseHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/device")
+public class DeviceController {
+    @Autowired
+    private DeviceService deviceService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Object> addDevice(@RequestBody Device device) {
+        try {
+            System.out.println(LocalDateTime.now());
+            return ResponseHandler.generateResponse("Successfully device added!", HttpStatus.OK, deviceService.addDevice(device));
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
+    }
+}
